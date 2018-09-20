99ead58e7aaf14bb84ae425e0bf65722acb06900
邵志明 <shaozhiming@172-1-1-163.lightspeed.hstntx.sbcglobal.net>
2018-09-20 18:37:22 +0800

    opt incr sync

--- cmd/incr-sync/main.go
+++ cmd/incr-sync/main.go
@@ -13,10 +13,16 @@ const (
 )
 
 var (
-	from uint64
+	from      uint64
+	height    uint64
+	curheight *uint64
 	//控制goroutine数量
 	chhandler  = make(chan bool, GONUM)
 	chblocknum = make(chan uint64, HANDLERNUM)
+	//历史区块同步超越标识
+	passflag = make(chan bool, 1)
+	//历史区块同步执行标识
+	execflag = true
 )
 
 func sync(n uint64) {
@@ -34,8 +40,27 @@ func sync(n uint64) {
 	}
 }
 
+func push2chblocknum(blocknum uint64) {
+	if pass := <-passflag; pass {
+		chblocknum <- blocknum
+		passflag <- true
+		return
+	}
+	height = blocknum
+	if execflag {
+		execflag = false
+		for i := from; i <= *curheight; i++ {
+			chblocknum <- i
+		}
+		passflag <- true
+	}
+}
+
 func main() {
 	chaininfo, _ := baas.GetChainInfo()
+	height = chaininfo.GetHeight()
+	curheight = &height
+	passflag <- false
 	eventClient, _ := baas.GetBlockEventClient()
 	reg, blockEventCh, _ := eventClient.RegisterBlockEvent()
 	defer eventClient.Unregister(reg)
@@ -52,9 +77,18 @@ func main() {
 		chhandler <- true
 	}
 
+	// go func() {
+	// 	for i := from; i <= height; i++ {
+	// 		chblocknum <- i
+	// 	}
+	// }()
+
 	go func() {
-		for i := from; i <= chaininfo.GetHeight(); i++ {
-			chblocknum <- i
+		for {
+			select {
+			case blockEvent := <-blockEventCh:
+				push2chblocknum(blockEvent.Block.Header.Number)
+			}
 		}
 	}()
 
@@ -68,9 +102,10 @@ func main() {
 					chhandler <- true
 				}()
 			}
-		case blockEvent := <-blockEventCh:
-			fmt.Printf("received block event: add block (%d)\n", blockEvent.Block.Header.Number)
-			go sync(blockEvent.Block.Header.Number)
+			// case blockEvent := <-blockEventCh:
+			// fmt.Printf("received block event: add block (%d)\n", blockEvent.Block.Header.Number)
+			// go sync(blockEvent.Block.Header.Number)
+
 		}
 	}
 }
